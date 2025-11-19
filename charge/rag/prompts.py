b3f8e87c0c5b4c07944bf23ace310bc0b5f4c89a
Tim Hsu <timhsu4@gmail.com>
2025-11-19 14:06:10 -0800

    add RAGv3 reaction prompt class

--- charge/rag/prompts.py
+++ charge/rag/prompts.py
@@ -1,68 +1,60 @@
 from dataclasses import dataclass
 
 
-@dataclass
 class ReactionDataPrompt:
-    role: str
-    task: str
-    instruction: str
-    output_format: str
-    input_data: str
-
     def __init__(self, forward : bool) -> None:
         """
         Args:
             forward (bool): whether the prompt is for forward synthesis context
         """
-        self.role = 'You are an expert chemist.'
-        self.task = 'Forward synthesis prediction' if forward else 'Retrosynthesis prediction'
-        self.instruction = \
-            "Given the input data in [INPUT DATA], perform your task and make your prediction, which must follow [OUTPUT FORMAT]."
-        self.output_format = ''.join([
-            "A single line of JSON string formatted as:\n",
-            '{"products": [...]}' if forward else '{"reactants": [...], "reagents": [...], "solvents": [...]}',
-            ",\n",
-            "where [...] is a list of SMILES strings.",
+        self.sections = dict()
+        self.sections['role'] = 'You are an expert chemist.'
+        self.sections['task'] = 'Forward synthesis prediction' if forward else 'Retrosynthesis prediction'
+        self.sections['instruction'] = \
+            "Given the input data in [INPUT DATA], perform your task and make your predictions, which must follow [OUTPUT FORMAT]."
+        self.sections['output format'] = ''.join([
+            "Each prediction must be a JSON string on a newline, formatted as: ",
+            '{\"products\": [...]}' if forward else '{\"reactants\": [...], \"agents\": [...], \"solvents\": [...]}',
+            ", ",
+            "where [...] is a list of SMILES strings. ",
+            "In case of forward synthesis the products can often be a list of a single molecule. ",
+            "In case of retrosynthesis the agents and the solvents can be absent."
         ])
-        self.input_data = ''
 
-    def to_string(self) -> None:
+    def to_string(self) -> str:
         strings = []
-        for k, v in self.__dict__.items():
-            section_title = f"[{k.replace('_', ' ').upper()}]"
+        for k, v in self.sections.items():
+            section_title = f'[{k.upper()}]'
             strings.append(section_title + '\n' + str(v) + '\n\n')
         return ''.join(strings)
 
-    def __str__(self) -> None:
+    def __str__(self) -> str:
         return self.to_string()
 
+    def __repr__(self) -> str:
+        sections_str = ', '.join([f'{k} = {v}' for k, v in self.sections.items()])
+        return f'{self.__class__.__name__}({sections_str})'
 
-@dataclass
+
 class ReactionDataPrompt_RAG(ReactionDataPrompt):
-    similar_reactions: str
-    
     def __init__(self, forward: bool) -> None:
         super().__init__(forward=forward)
-        self.instruction = (
+        self.sections['instruction'] = (
             "Given the input data in [INPUT DATA], a list of similar reactions is provided in [SIMILAR REACTIONS].\n"
             "Consider these similar reactions and make you prediction, which must follow [OUTPUT FORMAT]."
         )
-        self.similar_reactions = ''
+        self.sections['similar reactions'] = ''
 
 
-@dataclass
 class ReactionDataPrompt_RAGv2(ReactionDataPrompt):
-    expert_predictions: str
-    support_data: str
-    
     def __init__(self, forward: bool) -> None:
         super().__init__(forward=forward)
         input_role = 'reactants' if forward else 'products'
         output_role = 'products' if forward else 'reactants'
-        self.instruction = ''.join([
+        self.sections['instruction'] = ''.join([
             "Given the input data in [INPUT DATA], a list of expert predictions is provided in [EXPERT PREDICTIONS], ",
             "and a list of support examples is provided in [SUPPORT EXAMPLES].\n",
-            f"Each line in [EXPERT PREDICTIONS] is an prediction for [INPUT DATA], and is based on an expert chemical reaction model.\n",
+            f"Each line in [EXPERT PREDICTIONS] is a prediction for [INPUT DATA], and is based on an expert chemical reaction model.\n",
             "Each line in [SUPPORT EXAMPLES] consists of three columns:\n",
             f"(1) {input_role} similar to the input data, retrieved from a chemical reaction database,\n",
             f"(2) ground truth {output_role} for column (1), retrieved from the same database,\n",
@@ -72,5 +64,21 @@ class ReactionDataPrompt_RAGv2(ReactionDataPrompt):
             "construct a corrected or hybrid prediction guided by [SUPPORT EXAMPLES] and your chemistry expertise. ",
             "Your prediction output must follow [OUTPUT FORMAT].",
         ])
-        self.expert_predictions = ''
-        self.support_examples = ''
+        self.sections['expert predictions'] = ''
+        self.sections['support examples'] = ''
+
+
+class ReactionDataPrompt_RAGv3(ReactionDataPrompt):
+    def __init__(self, forward: bool) -> None:
+        super().__init__(forward=forward)
+        self.sections['instruction'] = ''.join([
+            "You are given a data table in [DATA TABLE]. In this table, each row/line consists of three columns: \n",
+            "(1) reaction data input,\n",
+            "(2) ground truth output retrieved from a database,\n",
+            "(3) predicted output from an expert chemical reaction model.\n",
+            "In one line the ground truth output is missing as denoted by '???'. ",
+            "Make several predictions (e.g., 3 to 5 different predictions) for this missing value. ",
+            "Your predictions must follow [OUTPUT FORMAT]. ",
+            "Observe the pattern in the table and the expert predictions as guidance. ",
+            "Note that the expert predictions may or may not be biased.",
+        ])
